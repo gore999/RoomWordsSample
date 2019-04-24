807c41d210a881037a4af13f636b900d340522ce
Carlos <carloscoucho@gmail.com>
2019-04-24 12:52:07 +0200

    Task 5: Crear la clase RoomDatabase. Es la que se encargará de hacer las consultas.  Evita el trato directo empleando un SQLHelper. Esta clase se construirá con un patrón singleton, define una única instancia que es la que devuelve. Comprueba, usando synchronized, que no se está creando otra instancia en otro lugar, si es así, espera el desbloqueo y comprueba nuevamente.

---
+++ app/src/main/java/rodriguezfernandez/carlos/roomwordssample/WordRoomDatabase.java
@@ -0,0 +1,27 @@
+package rodriguezfernandez.carlos.roomwordssample;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {Word.class},version = 1,exportSchema = false)
+public abstract class WordRoomDatabase extends RoomDatabase {
+    private static WordRoomDatabase INSTANCE;
+    //Metodo para recuperar el Dao.
+    public abstract WordDao wordDao();
+    //Será una clase tipo singleton (una unica instancia para toda la app)
+    public static WordRoomDatabase getDatabase(final Context context){
+        if(INSTANCE==null){//Comprueba si hay una instancia ya de WordRoomDatabase
+            synchronized (WordRoomDatabase.class){//Comprueba si no existe un bloqueo, si se está creando ya otra instancia en otro sitio.
+                if(INSTANCE==null){//Si cuando acaba el bloqueo sigue no habiendo instancia, entonces la creamos con el Builder de Room.
+                    //Necesita el contexto de la aplicacion, la clase WordRoomDatabase y el nombre de la base de datos.
+                    INSTANCE= Room.databaseBuilder(context.getApplicationContext(),WordRoomDatabase.class,"word_database")
+                            .fallbackToDestructiveMigration()//Si hay migracion a una nueva base, destruimos los datos previos: )
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
