48e7fcdda24c12c32ae6f62c05e2492ca77ff0ad
Carlos <carloscoucho@gmail.com>
2019-04-24 12:22:42 +0200

    Task 4:LiveData Hacemos que en vez de devolver directamente una lista en el método getAllWords, devuelva un LiveData que contenga a esa lista.

--- app/src/main/java/rodriguezfernandez/carlos/roomwordssample/WordDao.java
+++ app/src/main/java/rodriguezfernandez/carlos/roomwordssample/WordDao.java
@@ -1,5 +1,6 @@
 package rodriguezfernandez.carlos.roomwordssample;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
@@ -13,5 +14,5 @@ public interface WordDao {
     @Query("DELETE FROM word_table")
     void deleteAll();
     @Query("SELECT * FROM word_table ORDER BY word ASC")
-    List<Word> getAllWords();
+    LiveData<List<Word>> getAllWords();
 }
