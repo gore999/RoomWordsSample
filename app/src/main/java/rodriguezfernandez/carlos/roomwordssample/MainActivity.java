80f6b0d4eb1ef306d699dff06b8226ea31aeb8fb
Carlos <carloscoucho@gmail.com>
2019-04-25 15:29:43 +0200

    Task 12: Crear una nueva actividad encargada de añadir nuevas palabras. Se ajustan los strings y estilos que se necesitaran, creamos el layout y añadimos al botón la función de reconocer el texto contenido en el textview de la activity. -	Si el contenido es nulo, se manda un Result Canceled, -	Si el contenido es texto, se envía el resultado. Por ultimo, en ambos casos, se finaliza la activity.

--- app/src/main/java/rodriguezfernandez/carlos/roomwordssample/MainActivity.java
+++ app/src/main/java/rodriguezfernandez/carlos/roomwordssample/MainActivity.java
@@ -3,6 +3,7 @@ package rodriguezfernandez.carlos.roomwordssample;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -14,10 +15,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int NEW_WORD_ACTIVITY_REQUEST_CODE =1 ;
     private WordViewModel mWordViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,28 +28,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // Crear el recyclerView y asignarle un adaptador.
+        RecyclerView recyclerView=findViewById(R.id.recyclerview);
+        final WordListAdapter adapter=new WordListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         //Obtener el ViewModel desde ViewModelProviders.
         mWordViewModel=ViewModelProviders.of(this).get(WordViewModel.class);
         //Añadir el observer de livedata.
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        RecyclerView recyclerView=findViewById(R.id.recyclerview);
-        final WordListAdapter adapter=new WordListAdapter(this);
         mWordViewModel.getmAllWords().observe(this, new Observer<List<Word>>() {
             @Override
             public void onChanged(@Nullable List<Word> words) {
                 adapter.setWords(words);
             }
         });
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewWordActivity.class);
+                startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+            }
+        });
+
+
+
     }
 
     @Override
@@ -69,5 +75,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode==RESULT_OK){
+            Word word=new Word(data.getStringExtra(NewWordActivity.EXTRA_REPLY));
+            mWordViewModel.insert(word);
+        }else{
+            Toast.makeText(getApplicationContext(),R.string.empty_not_saved,Toast.LENGTH_LONG).show();
+        }
     }
 }
